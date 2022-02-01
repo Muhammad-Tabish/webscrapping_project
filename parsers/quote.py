0c2b5717617c1944f42f68274f4d29c9113509d7
tabishhameed <tabishhameed34@gmail.com>
2022-02-02 05:02:27 +0800

    Initial commit

---
+++ parsers/quote.py
@@ -0,0 +1,20 @@
+from locators.quotes_locators import QuoteLocators
+
+class QuoteParser:
+    def __init__(self, parent):
+        self.parent = parent
+
+    @property
+    def content(self):
+        locators = QuoteLocators.CONTENT
+        return self.parent.select_one(locators).string
+
+    @property
+    def author(self):
+        locators = QuoteLocators.AUTHOR
+        return self.parent.select_one(locators).string
+
+    @property
+    def tags(self):
+        locators = QuoteLocators.TAGS
+        return [e.string for e in self.parent.select(locators)]
